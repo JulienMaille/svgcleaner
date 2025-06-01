b41c522b8540f22424005c8c088a7b249fb375da
Callum Sykes <callumsykes@protonmail.com>
2025-06-01 18:52:53 +0200

    Fixes a font data issue

--- fixer/src/fixes/font_data.rs
+++ fixer/src/fixes/font_data.rs
@@ -8,11 +8,11 @@ pub struct FontData {
 // Delete <style type="text/css">@import url('https://themes.googleusercontent.com/fonts/css?family=Open Sans:400,600');</style>
 impl Task for FontData {
   fn fix(&self, svg: &String) -> String {
-    let re = Regex::new(r#"font-variant-ligatures="none" *"#).unwrap();
+    let re = Regex::new(r#"style="font-variant-ligatures:none" *"#).unwrap();
     let cow = re.replace_all(&svg, "");
     return cow.into_owned();
   }
-}
+} 
 
 #[cfg(test)]
 mod tests {
@@ -20,7 +20,7 @@ mod tests {
 
   #[test]
   fn test_remove_no_end_space() {
-    let svg = String::from(r#"<svg font-variant-ligatures="none"/>"#);
+    let svg = String::from(r#"<svg style="font-variant-ligatures:none"/>"#);
     let data = FontData {};
 
     let svg_out = data.fix(&svg);
@@ -30,7 +30,7 @@ mod tests {
 
   #[test]
   fn test_remove_end_space() {
-    let svg = String::from(r#"<svg font-variant-ligatures="none" />"#);
+    let svg = String::from(r#"<svg style="font-variant-ligatures:none" />"#);
     let data = FontData {};
 
     let svg_out = data.fix(&svg);
