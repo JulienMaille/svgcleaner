ac47adb159e29888d89203beed5e349d07fe4c12
Callum Sykes <callumsykes@protonmail.com>
2025-06-01 23:52:37 +0200

    Added comparison tests

--- compare/src/comparison.rs
+++ compare/src/comparison.rs
@@ -29,7 +29,7 @@ pub fn compare_svgs(left_svg_data: &String, right_svg_data: &String) {
     }
 
     if count > 0 {
-        panic!("Svgs are NOT equal ~ {} : {} != {}", count, left_svg_data, right_svg_data);
+        panic!("Svgs are NOT equal ~ {}/4096", count);
     }
 
     std::process::exit(0);
@@ -52,16 +52,28 @@ fn get_pixmap(svg_data:&String, pixmap:&mut PixmapMut) {
 
 #[cfg(test)]
 mod tests {
-
+  use test_case::test_case;
   use crate::compare_svgs;
 
-  #[test]
-  fn compare_simple_svg() {
-
-    let original = include_str!("../data/original_add.svg").to_string();
-    let cleaned = include_str!("../data/cleaned_add.svg").to_string();
-
-    compare_svgs(&original, &cleaned);
+  #[test_case(include_str!("../data/back_new.svg"), include_str!("../data/back_old.svg"))]
+  #[test_case(include_str!("../data/KeyboardShortcuts_darkmode_new.svg"), include_str!("../data/KeyboardShortcuts_darkmode_old.svg"))]
+  #[test_case(include_str!("../data/KeyboardShortcuts_new.svg"), include_str!("../data/KeyboardShortcuts_old.svg"))]
+  #[test_case(include_str!("../data/LayoutMenu_new.svg"), include_str!("../data/LayoutMenu_old.svg"))]
+  #[test_case(include_str!("../data/link_darkmode_new.svg"), include_str!("../data/link_darkmode_old.svg"))]
+  #[test_case(include_str!("../data/link_new.svg"), include_str!("../data/link_old.svg"))]
+  #[test_case(include_str!("../data/MacStatusBarHistory_new.svg"), include_str!("../data/MacStatusBarHistory_old.svg"))]
+  #[test_case(include_str!("../data/NewDetailOff_darkmode_new.svg"), include_str!("../data/NewDetailOff_darkmode_old.svg"))]
+  #[test_case(include_str!("../data/NewDetailOff_new.svg"), include_str!("../data/NewDetailOff_old.svg"))]
+  #[test_case(include_str!("../data/NewDetailOn_darkmode_new.svg"), include_str!("../data/NewDetailOn_darkmode_old.svg"))]
+  #[test_case(include_str!("../data/NewDetailOn_new.svg"), include_str!("../data/NewDetailOn_old.svg"))]
+  #[test_case(include_str!("../data/original_add_new.svg"), include_str!("../data/original_add_old.svg"))]
+  #[test_case(include_str!("../data/Properties_new.svg"), include_str!("../data/Properties_old.svg"))]
+  #[test_case(include_str!("../data/Save_darkmode_new.svg"), include_str!("../data/Save_darkmode_old.svg"))]
+  #[test_case(include_str!("../data/SVG_Editor_EyeDropper_darkmode_new.svg"), include_str!("../data/SVG_Editor_EyeDropper_darkmode_old.svg"))]
+  #[test_case(include_str!("../data/SVG_Editor_Pencil_darkmode_new.svg"), include_str!("../data/SVG_Editor_Pencil_darkmode_old.svg"))]
+  #[test_case(include_str!("../data/SVG_Editor_Pencil_new.svg"), include_str!("../data/SVG_Editor_Pencil_old.svg"))]
+  fn compare_simple_svg_asdsd(a: &str, b: &str) {
+    compare_svgs(&a.to_string(), &b.to_string());
   }
 
 }
